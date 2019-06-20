ad8c0366c2211585f291bbc5ad1782fa074c7a16
haydenadams <haydenzadams@gmail.com>
2019-06-20 14:24:04 -0400

    unisocks pass the smell test

--- test/unitests/test_unisocks.py
+++ test/unitests/test_unisocks.py
@@ -17,11 +17,14 @@ def test_init(w3, SOCKS, UNISOCKS):
 
 def test_transfer(w3, SOCKS, UNISOCKS, assert_fail):
     a0, a1, a2 = w3.eth.accounts[:3]
+    # Fail if no SOCKS are burned
     assert_fail(lambda: UNISOCKS.mint(a1, transact={}))
     assert SOCKS.burn(2*DECIMALS, transact={})
     UNISOCKS.mint(a1, transact={})
     assert UNISOCKS.totalSupply() == 1
     UNISOCKS.mint(a2, transact={})
+    # Fail if no more SOCKS are burned
+    assert_fail(lambda: UNISOCKS.mint(a1, transact={}))
     assert UNISOCKS.totalSupply() == 2
     assert UNISOCKS.balanceOf(a1) == 1
     assert UNISOCKS.balanceOf(a2) == 1
@@ -37,4 +40,19 @@ def test_transfer(w3, SOCKS, UNISOCKS, assert_fail):
     # Fails if not enough SOCKS are burned
     assert_fail(lambda: UNISOCKS.mint(a1, transact={}))
     # Test transfer by owner
-    # UNISOCKS.transfer(a2, transact={'from': a1})
+    UNISOCKS.transferFrom(a1, a2, 0, transact={'from': a1})
+    assert UNISOCKS.balanceOf(a1) == 0
+    assert UNISOCKS.balanceOf(a2) == 2
+    assert UNISOCKS.ownerOf(0) == a2
+    assert UNISOCKS.tokenOfOwnerByIndex(a2, 1) == 0
+    # Fails if transfer by wrong owner without approve
+    assert_fail(lambda: UNISOCKS.transferFrom(a2, a1, 1, transact={'from': a1}))
+    # Can't approve tokens you don't own
+    assert_fail(lambda: UNISOCKS.approve(a1, 1, transact={'from': a1}))
+    # Approve and transfer
+    UNISOCKS.approve(a1, 1, transact={'from': a2})
+    UNISOCKS.transferFrom(a2, a1, 1, transact={'from': a1})
+    assert UNISOCKS.balanceOf(a1) == 1
+    assert UNISOCKS.balanceOf(a2) == 1
+    assert UNISOCKS.ownerOf(1) == a1
+    assert UNISOCKS.tokenOfOwnerByIndex(a1, 0) == 1
